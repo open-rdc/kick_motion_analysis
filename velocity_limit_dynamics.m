48768c120c43e5a6a146556ba2f1eaee88bbc19a
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2019-02-25 00:19:19 +0900

    add d_gain

--- velocity_limit_dynamics.m
+++ velocity_limit_dynamics.m
@@ -2,7 +2,7 @@ clear;
 clf;
 
 %parameter
-height = 0.19 % (m)
+height = 0.20 % (m)
 stroke = 0.20 % (m)
 motor_velocity = 4.81 % (rad/s) 46rpm
 l1 = 0.108 % (m) length of link
@@ -11,11 +11,14 @@ ratio = 0.05; % display
 P = [0, 0; 0, 0.1; 0.3, 0.1; stroke, 0.0];
 period = 0.30 % (s)
 cog = 0.3 % (m) center of gravity
-m1 = 0.5;
-m2 = 0.5;
-I1 = 1/12 * m1 * l1^2;
-I2 = 1/12 * m2 * l2^2;
-gain = 50.0;
+%m1 = 0.23;
+%m2 = 0.35;
+m1 = 0.30;
+m2 = 0.50;
+I1 = 1/4 * m1 * l1^2;
+I2 = 1/4 * m2 * l2^2;
+p_gain = 50.0;
+d_gain = 0.0;
 max_torque = 4.6; % (Nm)
 
 %parameter low
@@ -47,12 +50,12 @@ endfunction
 
 function [t1, t2, t1d, t2d] = dynamics(t1, t2, t1d, t2d, f1, f2, l1, l2, m1, m2, I1, I2, motor_velocity, dt)
   g = 9.8;
-  M = [(l1^2*m1+4*l1^2*m2+4*I1)/4 (l1*l2*m2*cos(t2-t1))/2;
-       (l1*l2*m2*cos(t2-t1))/2    (2*l2^2*m2+8*I2)/8];
-  D = [((l1*l2*m2*t2d^2-l1*l2*m2*t1d*t2d)*sin(t2-t1))/2;
-       ((l1*l2*m2*t1d*t2d-l1*l2*m2*t1d^2)*sin(t2-t1))/2];
-  G = [g*l1*m2*cos(t1)+(g*l1*m1*cos(t1))/2;
-       (g*l2*m2*cos(t2))/2];
+  M = [I1+l1^2*m2+l1^2*m1 l1*l2*m2*cos(t2-t1);
+       l1*l2*m2*cos(t2-t1) (2*l2^2*m2+2*I2)/2];
+  D = [(l1*l2*m2*t1d*t2d-l1*l2*m2*t2d^2)*sin(t2-t1);
+       (l1*l2*m2*t1d^2-l1*l2*m2*t1d*t2d)*sin(t2-t1)];
+  G = [g*l1*m2*cos(t1)+g*l1*m1*cos(t1);
+       g*l2*m2*cos(t2)];
   tdd = inv(M) * [[f1; f2] - D - G];
   t1dd = tdd(1);
   t2dd = tdd(2);
@@ -62,17 +65,22 @@ function [t1, t2, t1d, t2d] = dynamics(t1, t2, t1d, t2d, f1, f2, l1, l2, m1, m2,
   t2 = t2 + t2d * dt + 1 / 2 * t2dd * dt^2;
 endfunction
 
-xr = -stroke/4;
-yr = -height;
-[t1r, t2r] = inverse_kinematics(xr, yr, l1, l2);
-t1rd = 0; t2rd = 0;
-
-i = 0;
-dt = 0.001;
 g = 9.8;
 Tc = sqrt(cog/g)
 x0 = -stroke/4;
 v0 = stroke/4*(cosh(period/Tc)+1)/(Tc*sinh(period/Tc))
+
+xr = -stroke/4;
+yr = -height;
+[t1r, t2r] = inverse_kinematics(xr, yr, l1, l2);
+t1rd = 0; t2rd = 0;
+jaco = [-l1*sin(t1r) -l2*sin(t2r); l1*cos(t1r) l2*cos(t2r)];
+trd = inv(jaco) * [-v0; 0];
+t1rd = trd(1)
+t2rd = trd(2)
+
+i = 0;
+dt = 0.001;
 
 for t = 0.0: dt/period: 2 * period/period
   sup_pos = x0 * cosh(mod(t,1) * period / Tc) + Tc * v0 * sinh(mod(t,1) * period / Tc) - x0;
@@ -91,10 +99,13 @@ for t = 0.0: dt/period: 2 * period/period
   endif
   [t1, t2] = inverse_kinematics(x - sup_leg_pos, y, l1, l2);
   w1r = -t1rd/motor_velocity;
-  f1 = max([min([gain * (t1 - t1r), max_torque * (1 + w1r)]), -max_torque * (1 - w1r)]);
+  f1 = max([min([p_gain * (t1 - t1r) - d_gain * t1rd, max_torque * (1 + w1r)]), -max_torque * (1 - w1r)]);
   w2r = -t2rd/motor_velocity;
-  f2 = max([min([gain * (t2 - t2r), max_torque * (1 + w2r)]), -max_torque * (1 - w2r)]);
+  f2 = max([min([p_gain * (t2 - t2r) - d_gain * t2rd, max_torque * (1 + w2r)]), -max_torque * (1 - w2r)]);
   [t1r, t2r, t1rd, t2rd] = dynamics(t1r, t2r, t1rd, t2rd, f1, f2, l1, l2, m1, m2, I1, I2, motor_velocity, dt);
+%  printf("%f %f %f %f %f %f %f %f %f %f\n",
+%    t1r, t1rd, f1, max_torque * (1 + w1r), -max_torque * (1 - w1r),
+%    t2r, t2rd, f2, max_torque * (1 + w2r), -max_torque * (1 - w2r));
   
 %  t1r = t1r + max([min([t1 - t1r, motor_velocity * dt]), -motor_velocity * dt]);
 %  t2r = t2r + max([min([t2 - t2r, motor_velocity * dt]), -motor_velocity * dt]);
